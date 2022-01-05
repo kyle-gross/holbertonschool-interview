c18ba7c7124f936b451f05a3f6cbf309793c1da7
Kyle Gross <kyg555@outlook.com>
2022-01-05 15:32:11 -0800

    Reformat style, itoa func was too buggy for long ints.

--- 0x07-palindrome_integer/0-is_palindrome.c
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -7,93 +7,18 @@
  */
 int is_palindrome(unsigned long n)
 {
-	char buffer[1024];
-	int len = 0, j = 0, i = 0;
+	unsigned long original = n, new_n = 0, x = 0;
 
-	itoa(n, buffer, 10);
-	len = strlen(buffer);
-	
-	if (len == 0 || len == 1)
-		return (1);
-
-	j = len / 2;
-	i = j;
-
-	if (len % 2 != 0)
+	while (n / 10 > 0)
 	{
-		j++;
-		i--;
+		x = n % 10;
+		new_n = (10 * new_n) + x;
+		n /= 10;
 	}
-	else
-		i = j - 1;
+	new_n = (10 * new_n) + x;
 
-	for (; i >= 0; i--)
-	{
-		if (buffer[i] != buffer[j])
-			return (0);
-		j++;
-	}
+	if (original != new_n)
+		return (0);
 
 	return (1);
-}
-
-/**
- * rev_string - reverses a string
- * @s: the string to reverse
- * Return: void
- */
-void rev_string(char *s)
-{
-	int c, i, len = strlen(s);
-
-	for (i = 0, c = len - 1; i < c; i++, c--)
-	{
-		char tmp = s[i];
-
-		s[i] = s[c];
-		s[c] = tmp;
-	}
-}
-
-
-/**
- * itoa - converts integer to string
- * @num: number to convert
- * @str: buffer of string
- * @base: decimal place to convert to
- * Return: converted string
- */
-char* itoa(int num, char* str, int base)
-{
-    int i = 0;
-    bool isNegative = false;
- 
-    if (num == 0)
-    {
-        str[i++] = '0';
-        str[i] = '\0';
-        return str;
-    }
- 
-    if (num < 0 && base == 10)
-    {
-        isNegative = true;
-        num = -num;
-    }
- 
-    while (num != 0)
-    {
-        int rem = num % base;
-        str[i++] = (rem > 9)? (rem-10) + 'a' : rem + '0';
-        num = num/base;
-    }
- 
-    if (isNegative)
-        str[i++] = '-';
- 
-    str[i] = '\0';
- 
-    rev_string(str);
- 
-    return (str);
 }
--- 0x07-palindrome_integer/palindrome.h
+++ 0x07-palindrome_integer/palindrome.h
@@ -4,12 +4,8 @@
 /* INCLUDES */
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdbool.h>
 
 /* FUNCTIONS */
 int is_palindrome(unsigned long n);
-char* itoa(int num, char* str, int base);
-void rev_string(char *s);
 
 #endif
