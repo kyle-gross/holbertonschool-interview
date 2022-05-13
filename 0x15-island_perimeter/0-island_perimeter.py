1e4aa98a9f92886dee764d9ee9a29d979a1e0ebb
Kyle Gross <kyg555@outlook.com>
2022-05-12 20:35:52 -0700

    add project

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Finds the perimeter of an island"""
+
+def island_perimeter(grid):
+    """ Let's find the perimeter of an island! :D """
+
+    # Null check
+    if grid is None:
+        return 0
+
+    perim = 0
+    row_len = len(grid)
+    col_len = len(grid[0])
+
+    # Verify that grid is right size
+    if row_len > 100 or col_len > 100:
+        return 0
+
+    # Loop through grid and look around when 1's are encountered
+    # If there's 0's or matrix edge around a 1, perimeter increases
+    try:
+        for row in range(row_len):
+            for col in range(col_len):
+                if grid[row][col] == 1:
+                    if grid[row - 1][col] == 0:
+                        perim += 1
+                    if grid[row + 1][col] == 0:
+                        perim += 1
+                    if grid[row][col - 1] == 0:
+                        perim += 1
+                    if grid[row][col + 1] == 0:
+                        perim += 1
+    except IndexError:
+        continue
+    return perim
